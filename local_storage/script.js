ce1d7aea6f45017b6d064fe1f95d73ad7d414563
Aarya Ghate <aaryaaghate@gmail.com>
2024-05-03 20:46:38 +0530

    first commit

---
+++ local_storage/script.js
@@ -0,0 +1,47 @@
+document.getElementById("inputform").addEventListener("submit",function(event){
+    event.preventDefault();
+
+    var username=document.getElementById('username').value;
+    var email=document.getElementById("email").value;
+    var password=document.getElementById("password").value;
+    var confirm_password=document.getElementById("confirm_password").value;
+    var phoneno=document.getElementById("phnumber").value;
+    var gender=document.getElementById("gender").value;
+
+    if(confirm_password!==password){
+        alert("Sorry the password does not match ");
+        return;
+    }
+
+    if(!(/^\d{10}$/.test(phoneno))){
+        alert("phone no should have 10 digits");
+        return;
+    }
+
+    var userData={
+        username:username,
+        email:email,
+        password:password,
+        phoneno:phoneno,
+        gender:gender
+    }
+
+
+    if(typeof(Storage)!=="undefined"){
+
+        var users=JSON.parse(localStorage.getItem("users"))||[];
+        users.push(userData);
+
+        localStorage.setItem("users",JSON.stringify(users));
+
+        window.location.href="user_list.html";
+    }
+
+    else{
+        alert("you browser does not support local storage")
+    }
+})
+
+document.getElementById("viewlist").addEventListener("click",function(){
+    window.location.href="user_list.html";
+})
